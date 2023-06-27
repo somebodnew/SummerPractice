ae1e39e5742555090044014cc507d05d35d80b9f
zaryanezrya <ivan@sha.run>
2023-06-27 11:45:15 +0600

    Доавлен тест для сервиса 1

--- SalonLib.Test/ServiceLocatorTest.cs
+++ SalonLib.Test/ServiceLocatorTest.cs
@@ -5,7 +5,7 @@ public class ServiceLocatorTest
     [Fact]
     public void Service1Test()
     {
-        var expected = "В читальном зале";
+        var expected = "В кабинете";
         var actual = SalonLib.ServiceLocator.GetService("Читать стихи");
         Assert.Equal(expected, actual);
     }
