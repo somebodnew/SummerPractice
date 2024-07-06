15c2d17307c9a7c4a87d23abb5f22b7fa6e36d87
Timur | IronSmail <51315799+somebodnew@users.noreply.github.com>
2024-07-06 11:18:38 +0600

    Update ServiceLocatorTest.cs

--- SalonLib.Test/ServiceLocatorTest.cs
+++ SalonLib.Test/ServiceLocatorTest.cs
@@ -5,7 +5,7 @@ public class ServiceLocatorTest
     [Fact]
     public void Service1Test()
     {
-        var expected = "В кабинете";
+        var expected = "В читальном зале";
         var actual = SalonLib.ServiceLocator.GetService("Читать стихи");
         Assert.Equal(expected, actual);
     }
@@ -13,7 +13,7 @@ public class ServiceLocatorTest
     [Fact]
     public void Service2Test()
     {
-        var expected = "В читальном зале";
+        var expected = "У рояля";
         var actual = SalonLib.ServiceLocator.GetService("Петь романсы");
         Assert.Equal(expected, actual);
     }
@@ -21,7 +21,7 @@ public class ServiceLocatorTest
     [Fact]
     public void Service3Test()
     {
-        var expected = "В читальном зале";
+        var expected = "В кабинете";
         var actual = SalonLib.ServiceLocator.GetService("Писать статьи");
         Assert.Equal(expected, actual);
     }
